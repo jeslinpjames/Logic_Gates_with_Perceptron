946b622c8f22d5fab5362dfe330b6c298d05b7f1
jeslinpjames <jeslinpjames007@gmail.com>
2023-10-20 00:32:53 +0530

    added back-prop part

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<cstdlib>
+#include<iomanip>
 using namespace std;
 class Perceptron{
     private:
         float *W;
         float b;
         int input_dim;
-        int iteration;
+        int iterations;
         int learning_rate;
     public:
         ~Perceptron() {}
@@ -14,15 +15,19 @@ class Perceptron{
         float forward_prop(float x[]);
         float activation(float x);
         float getRandom(){}
+        void printPrediction(float pred[],float y[]);
+        void train(float x[][input_dim],float y[],int num_samples);
+        float* predict(float x[][input_dim],int num_samples);
+        void describe();
 };
-Perceptron::Perceptron(int input_dim,int iteration=100,int learning_rate=0.1){
+Perceptron::Perceptron(int input_dim,int iterations=100,int learning_rate=0.1){
     this->input_dim = input_dim;
     W= new float[input_dim];
     for(int i=0;i<input_dim;i++){
         W[i] = getRandom();
     }
     b = getRandom();
-    this->iteration = iteration;
+    this->iterations = iterations;
     this->learning_rate = learning_rate;
 }
 Perceptron::~Perceptron() {
@@ -48,4 +53,54 @@ float Perceptron::activation(float x){
     else{
         return 0.0;
     }
+}
+void Perceptron::printPrediction(float pred[],float y[])
+{
+	cout<<"\n\n\tPREDICTIONS\n\n";
+	cout<<setw(10)<<"  actual   |"<<setw(10)<<"   predicted"<<endl;
+	cout<<setw(10)<<"-----------|"<<setw(10)<<"------------"<<endl;
+    for(int i=0;i<4;i++)
+        cout<<setw(6)<<y[i]<<"     |"<<setw(8)<<"   "<<pred[i]<<endl;
+}
+void Perceptron::train(float x[][input_dim],float y[],int num_samples){
+    int j =0;
+    while(j<=iterations){
+        int noChange =0;
+        for(int i=0;i<num_samples;i++){
+            float ypred = forward_prop(x[i]);
+            if(ypred>0 && y[i]==1){
+                for(int k=0;k<input_dim;k++)    
+                    W[k] -= learning_rate*x[i][k];
+            }
+            else if(ypred<0 && y[i]==0){
+                for(int k=0;k<input_dim;k++)    
+                    W[k] += learning_rate*x[i][k];
+            }
+            else{
+                noChange++;
+            }
+        }
+        if(noChange==num_samples){
+            break;
+        }   
+    }
+}
+float* Perceptron::predict(float x[][input_dim],int num_samples){
+    float *pred = new float[num_samples];
+    for(int i=0;i<num_samples;i++){
+        pred[i] = activation(forward_prop(x[i]));
+    }
+    return pred;
+}
+void Perceptron::describe() {
+    cout << "Perceptron Parameters:\n";
+    cout << "---------------------\n";
+    cout << "Input Dimensions: " << input_dim << endl;
+    cout << "Iterations: " << iterations << endl;
+    cout << "Learning Rate: " << learning_rate << endl;
+    cout << "Weights (W): ";
+    for (int i = 0; i < input_dim; i++) {
+        cout << "W[" << i << "] = " << W[i] << " ";
+    }
+    cout << "\nBias (b): " << b << endl;
 }
